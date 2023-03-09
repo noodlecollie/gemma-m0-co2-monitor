6c5906c9d59e12c88e984eb3d2cd5a86660c3b12
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-03-09 16:17:34 +0000

    Got it to beep

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 
+namespace PinNumber
+{
+	static constexpr uint32_t A0 = 1;
+	static constexpr uint32_t SINGLE_LED = 13;
+}
+
 void setup()
 {
-	pinMode(13, OUTPUT);
+	pinMode(PinNumber::SINGLE_LED, OUTPUT);
+	pinMode(1, OUTPUT);
 }
 
 void loop()
 {
-	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-	delay(1000);              // wait for a second
+	static bool isHigh = false;
+
+	digitalWrite(PinNumber::SINGLE_LED, isHigh ? HIGH : LOW);
+	analogWrite(PinNumber::A0, isHigh ? 999 : 0);
+
+	isHigh = !isHigh;
+	delay(1);
 }
