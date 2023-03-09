8841a94863c61c55a16c12758a11839a480333b6
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-03-09 16:30:21 +0000

    Got beeps to alternate

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,24 @@ namespace PinNumber
 void setup()
 {
 	pinMode(PinNumber::SINGLE_LED, OUTPUT);
-	pinMode(1, OUTPUT);
+	pinMode(PinNumber::A0, OUTPUT);
 }
 
 void loop()
 {
 	static bool isHigh = false;
+	static uint32_t count = 0;
 
 	digitalWrite(PinNumber::SINGLE_LED, isHigh ? HIGH : LOW);
-	analogWrite(PinNumber::A0, isHigh ? 999 : 0);
+	analogWrite(PinNumber::A0, (isHigh && count < 500) ? 255 : 0);
 
 	isHigh = !isHigh;
+	++count;
+
+	if ( count >= 1000 )
+	{
+		count = 0;
+	}
+
 	delay(1);
 }
